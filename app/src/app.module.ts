0cb6a771318ee0d668623ab2bc75628c5ed3ef37
Thiago Medeiros <thiago.medeiros@hollowsoft.com>
2024-10-18 12:18:57 -0300

    Integration mongoose (#2)

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { HealthModule } from './module/health/health.module'
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { HealthModule } from './module/health/health.module';
+import { RepositoryConfigProvider } from './repository.config.provider'
 
 
 @Module({
   imports: [
-    HealthModule
+    HealthModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({ useClass: RepositoryConfigProvider }),
   ],
   providers: [],
 })
--- app/src/module/health/health.controller.ts
+++ app/src/module/health/health.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('health')
 export class HealthController {
-
-    @Get()
-    getAllhealth() { }
+  @Get()
+  health() { }
 }
